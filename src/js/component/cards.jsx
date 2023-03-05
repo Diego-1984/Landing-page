433cdd34adc57906f0e829048d6315d04c3cea94
Diego-1984 <diegomenoruceta@gmail.com>
2023-03-05 10:26:04 +0000

    Proyecto terminado

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -1,27 +1,50 @@
 import React from "react";
 
-const Card=()=>{
+const Card=(props)=>{
     return(
     <>
     <div className="card mx-1 rounded my-3">
             <img className="card-img-top" src="https://loremflickr.com/350/350" alt="Card image cap" />
             <div className="card-body">
-                <h5 className="card-title d-flex justify-content-center">Card title</h5>
-                <p className="card-text d-flex text-center">Some quick example text to build on the card title and fill the card's content.</p>
-                <a href="#" className="btn btn-primary mx-5 my-5">Find Out More</a>
+                <h5 className="card-title d-flex justify-content-center">{props.title}</h5>
+                <p className="card-text d-flex text-center">{props.text}</p>
+                <a href="#" className="btn btn-primary my-3 mx-5 justify-content-center w-50">{props.buttom}</a>
             </div>
-        </div>
+    </div>
     </>
     )
 }
 const Cards=()=>{
     return(
         <>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        
+        <Card 
+            title="Hola" 
+            text="No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, 
+            simplemente porque es el dolor."
+            buttom="Pulsa aqui"/>
+
+        <Card 
+            title="¡Perfecto!" 
+            text="No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, 
+            simplemente porque es el dolor."
+            buttom="¡Dale duro!"/>
+
+        <Card 
+            title="Lo estoy logrando"
+            text="No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, 
+            simplemente porque es el dolor."
+            buttom="Haz click"/>
+
+        <Card 
+            title="¡Yuhuuu!" 
+            text="No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, 
+            simplemente porque es el dolor."
+            buttom="Mas info"/>
+        
         </>
     )
 }
+
+
 export default Cards;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,22 +1,19 @@
 import React from "react";
-
-
-//include images into your bundle
-
 import MyNavbar from "./navbar.jsx";
 import Jumbotron from "./jumbotron.jsx";
 import Cards from "./cards.jsx";
 import Footer from "./footer.jsx";
 
-//create your first component
+
 const Home = () => {
 	return (
 		<>
 		<MyNavbar/>
 		<div className="container">
-		<Jumbotron/>
-		<div className="card-group"><Cards/></div>
-		
+			<Jumbotron/>
+				<div className="card-group">
+					<Cards/>
+				</div>
 		</div>
 		<Footer/>
 		</>
